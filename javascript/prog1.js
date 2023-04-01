abe0a9ec9aa85ee66b23951af10c5934684b5575
saiteja <saitejakura17@gmail.com>
2023-04-01 11:31:35 +0530

    d

---
+++ javascript/prog1.js
@@ -0,0 +1,5 @@
+function printArray(arr){
+  //show me the code!
+  let s=arr.join(",");
+  return s;
+}
---
+++ javascript/prog3.js
@@ -0,0 +1,10 @@
+function basicOp(op, val1, val2)
+{
+  // Code
+  if(op=='+'){
+    return val1+val2;
+  }
+  else if(op=='-') return val1-val2;
+  else if(op=='*') return val1*val2;
+  else return val1/val2;
+}
